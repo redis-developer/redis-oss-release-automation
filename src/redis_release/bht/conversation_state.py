02b2d73a7f14079368f5d2ca8aa6ae14a68f2cf8
Petar Shtuchkin <petar.shtuchkin@redis.com>
2026-01-27 17:44:24 +0200

    Checkpoint

--- src/redis_release/bht/conversation_state.py
+++ src/redis_release/bht/conversation_state.py
@@ -2,7 +2,7 @@ from typing import List, Optional
 
 from pydantic import BaseModel, Field
 
-from ..conversation_models import Command, InboxMessage
+from ..conversation_models import Command, InboxMessage, LLMReleaseArgs
 from ..models import ReleaseArgs, SlackArgs, SlackFormat
 
 
@@ -13,6 +13,7 @@ class ConversationState(BaseModel):
     command: Optional[Command] = None
     command_started: bool = False
     is_confirmed: bool = False
+    llm_release_args: Optional[LLMReleaseArgs] = None
     release_args: Optional[ReleaseArgs] = None
     replies: List[str] = Field(default_factory=list)
 
