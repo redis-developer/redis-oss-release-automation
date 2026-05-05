6c5a5d24e8696667e483b23f038439d3b29ec4e6
Petar Shtuchkin <petar.shtuchkin@redis.com>
2026-05-05 17:16:28 +0300

    Fix prompt for pre 8

--- src/redis_release/conversation_models.py
+++ src/redis_release/conversation_models.py
@@ -65,11 +65,11 @@ INSTRUCTION_SNIPPETS = {
         Modules are always built together with redis since version 8, so it is not possible to build just a module or redis without building all the modules as well or to exclude certain module.
     """,
     "release_tags": """Release tags are either in version format or branch name format.and
-    Examples: 8.4-m01, 8.6-rc1, 8.4-int3, 8.2-rc2-int1, 8.0.2
+    Examples: 8.4-m01, 8.6-rc1, 8.4-int3, 8.2-rc2-int1, 8.0.2, 6.2.22, 7.4.2
     Versions without patch number and suffix, like 8.2 or 8.6 are inicating a branch and implies custom build.
     unstable is the default redis branch, could be also referred as nightly
     git sha could also be used as release tag, that would imply custom build as well.
-    Redis versions starting from 8 major versions are supported, other series cannot be built.
+    Redis versions starting from 6 major versions are supported
     """,
     "module_versions": """Module versions are either git tag names or branch names
     Note that module version COULD NOT BE A GIT SHA
